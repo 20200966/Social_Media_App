409798b144c04ae44b48045bfbe54024aa4e4cc0
20200966 <20200966@miucateci.edu.do>
2024-12-04 13:15:57 -0400

    feat: add email

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,28 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:social/components/my_textfield.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.surface,
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            //logo
-            Icon(
-              Icons.person,
-              size: 80,
-              color: Theme.of(context).colorScheme.inversePrimary,
-            ),
-
-            const Text(
-              "M I N I M A L",
-              style: TextStyle(fontSize: 20),
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(25.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              //logo
+              Icon(
+                Icons.person,
+                size: 80,
+                color: Theme.of(context).colorScheme.inversePrimary,
+              ),
+          
+              const Text(
+                "M I N I M A L",
+                style: TextStyle(fontSize: 20),
+              ),
+          
+              const SizedBox(height: 50),
+          
+              //email input
+              MyTextField(
+                hintText: "",
+                obscureText: false,
+                controller: emailController,
+              ),
+            ],
+          ),
         ),
       ),
     );
