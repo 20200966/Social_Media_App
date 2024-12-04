d1f6d04e8bd819f66e41689d8f6b697b83706005
20200966 <20200966@miucateci.edu.do>
2024-12-04 14:03:45 -0400

    feat: add register page

---
+++ lib/pages/register_page.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+import 'package:social/components/my_textfield.dart';
+
+import '../components/my_button.dart';
+
+class RegisterPage extends StatelessWidget {
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController confirmPwController = TextEditingController();
+
+  RegisterPage({super.key});
+
+  //register method
+  void register() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(25.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              //logo
+              Icon(
+                Icons.person,
+                size: 80,
+                color: Theme.of(context).colorScheme.inversePrimary,
+              ),
+          
+              const Text(
+                "M I N I M A L",
+                style: TextStyle(fontSize: 20),
+              ),
+          
+              const SizedBox(height: 50),
+          
+              //username input
+              MyTextField(
+                hintText: "Username",
+                obscureText: false,
+                controller: usernameController,
+              ),
+
+              const SizedBox(height: 10),
+
+              //email input
+              MyTextField(
+                hintText: "Email",
+                obscureText: false,
+                controller: emailController,
+              ),
+
+              const SizedBox(height: 10),
+
+              //password input
+              MyTextField(
+                hintText: "Password",
+                obscureText: true,
+                controller: passwordController,
+              ),
+
+              const SizedBox(height: 10),
+
+              //confirm password input
+              MyTextField(
+                hintText: "Confirm Password",
+                obscureText: true,
+                controller: confirmPwController,
+              ),
+
+              const SizedBox(height: 10),
+
+              //forgot password
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    "Forgot Password",
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.inversePrimary)
+                    ),
+                ],
+              ),
+
+              const SizedBox(height: 25),
+
+              MyButton(
+                text: "Register",
+                onTap: register,
+              ),
+
+              const SizedBox(height: 25),
+
+              //register
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Already have an account?",
+                  style: TextStyle(
+                    color: Theme.of(context).colorScheme.inversePrimary),
+                  ),
+                  
+                  GestureDetector(
+                    onTap: () {},
+                    child: const Text(
+                      "  Login",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              )
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
