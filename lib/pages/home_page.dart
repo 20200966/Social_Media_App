12d38edd15de24a0b197f0ca97da94356e6c91d3
20200966 <20200966@miucateci.edu.do>
2024-12-04 22:00:07 -0400

    feat: post message

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,11 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:social/components/my_drawer.dart';
 import 'package:social/components/my_post_button.dart';
 import 'package:social/components/my_textfield.dart';
+import 'package:social/database/firestore.dart';
 
 class HomePage extends StatelessWidget {
   HomePage({super.key});
 
+  //firestore access
+  final FirestoreDatabase database = FirestoreDatabase();
+
   final TextEditingController newPostController = TextEditingController();
+
+  //post message
+  void postMessage() {
+    //only post message if there is something in the textfield
+    if (newPostController.text.isNotEmpty) {
+      String message = newPostController.text;
+      database.addPost(message);
+    }
+
+    //clear the controller
+    newPostController.clear();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,7 +51,7 @@ class HomePage extends StatelessWidget {
 
                 //post button
                 PostButton(
-                  onTap: () {} 
+                  onTap: postMessage, 
                   )
               ],
             ),
