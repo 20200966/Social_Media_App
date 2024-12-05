535e74bd7d5279e8abcb20ba310cc37c776c5222
20200966 <20200966@miucateci.edu.do>
2024-12-04 22:30:14 -0400

    feat: Show posts

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:social/components/my_drawer.dart';
 import 'package:social/components/my_post_button.dart';
@@ -55,6 +56,52 @@ class HomePage extends StatelessWidget {
                   )
               ],
             ),
+          ),
+
+          //Posts
+          StreamBuilder(
+            stream: database.getPostsStream(), 
+            builder: (context, snapshot) {
+              //show loading circle
+              if(snapshot.connectionState == ConnectionState.waiting) {
+                return Center(child: CircularProgressIndicator());
+              }
+
+              //get all posts
+              final posts = snapshot.data!.docs;
+
+              //no data?
+              if(snapshot.data == null || posts.isEmpty) {
+                return const Center(
+                  child: Padding(
+                    padding: EdgeInsets.all(25),
+                    child: Text("No posts available... Post something!"),
+                    ),
+                  );
+                }
+
+               //return as a list
+               return Expanded(child: ListView.builder(
+                itemCount: posts.length,
+                itemBuilder: (context, index) {
+                  
+                  //get each individual post
+                  final post = posts[index];
+
+                  //get data from each post
+                  String message = post['PostMessage'];
+                  String userEmail = post['UserEmail'];
+                  Timestamp timestamp = post['TimeStamp'];
+
+                  //return as a list title
+                  return ListTile(
+                    title: Text(message),
+                    subtitle: Text(userEmail),
+                  );
+                }
+               ),
+              ); 
+            }
           )
         ],
       ),
