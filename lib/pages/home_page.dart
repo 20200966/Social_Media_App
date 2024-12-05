80cd7f91e6b6820c5be7b92fa2fb1ab7a734fb5f
20200966 <20200966@miucateci.edu.do>
2024-12-04 21:25:07 -0400

    feat: add bar to say things

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:social/components/my_drawer.dart';
+import 'package:social/components/my_textfield.dart';
 
 class HomePage extends StatelessWidget {
   HomePage({super.key});
@@ -9,12 +10,25 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
       appBar: AppBar(title: const Text("O M E G A"),
-      backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      backgroundColor: Colors.transparent,
+      foregroundColor: Theme.of(context).colorScheme.inversePrimary,
       elevation: 0,
-      
       ),
       drawer: const MyDrawer(),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(25),
+            child: MyTextField(
+              hintText: "Say something...",
+              obscureText: false,
+              controller: newPostController,
+            ),
+          )
+        ],
+      ),
     );
   }
 }
