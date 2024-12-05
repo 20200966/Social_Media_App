3c0d7c240f607c5c3fe46a07ebd1b5a7a3650ffb
20200966 <20200966@miucateci.edu.do>
2024-12-04 20:22:50 -0400

    feat: setting email/username

--- lib/pages/users_page.dart
+++ lib/pages/users_page.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:social/helper/helper_functions.dart';
 
 class UsersPage extends StatelessWidget {
   const UsersPage({super.key});
@@ -6,9 +8,40 @@ class UsersPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Users"),
+      appBar: AppBar(title: const Text("Users"),
       backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       elevation: 0,
+      ),
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection("Users").snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            displayMessageToUser("Something went wrong", context);
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          if (snapshot.data == null) {
+            return const Text("No data");
+          }
+
+          final users = snapshot.data!.docs;
+          return ListView.builder(
+            itemCount: users.length,
+            itemBuilder: (context, index) {
+              final user = users[index];
+              return ListTile(
+                title: Text(user['username']),
+                subtitle: Text(user['email']),
+              );
+            },
+          ); 
+        },
       ),
     );
   }
